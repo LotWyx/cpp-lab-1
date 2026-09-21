3ccb4b0552234492bf2c9c2c6d317ac100360381
LotWyx <lotwyx@gmail.com>
2026-09-21 16:29:07 +0700

    Fix sanitizer bugs

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,11 @@
 
 int main() {
     int* values = new int[3]{1, 2, 3};
-    std::cout << values[3] << '\n';
+    std::cout << values[0] << '\n';
     delete[] values;
 
     int a = 10;
-    int b = 0;
+    int b = 1;
     std::cout << a / b << '\n';
 
     return 0;
